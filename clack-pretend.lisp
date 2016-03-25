be3b7b36b0064112e136fd967df5bfa1a6cc9eab
Ben McGunigle <bnmcgn@gmail.com>
2016-03-25 08:02:23 -0700

    Bugfixes to date. Changes to work with clack v.2

--- clack-pretend.lisp
+++ clack-pretend.lisp
@@ -8,6 +8,7 @@
 (defparameter *pretend-storage-size* 10)
 (defvar *pretend-storage* nil)
 
+#|
 (defclass clack-pretend (lack.component:lack-component)
   ((watch-symbols :type list
                   :initarg :watch-symbols
@@ -24,6 +25,20 @@
   (let ((inner (call-next this env)))
     (setf (gethash :output (car *pretend-storage*)) inner)
     inner))
+|#
+
+(defun pretend-component (app watch-symbols)
+  (lambda (env)
+    (when (< *pretend-storage-size* (length *pretend-storage*))
+      (setf *pretend-storage*
+            (subseq *pretend-storage* 0 (1- *pretend-storage-size*))))
+    (push (make-hash-table) *pretend-storage*)
+    (setf (gethash :input (car *pretend-storage*)) env)
+    (dolist (sym watch-symbols)
+      (setf (gethash sym (car *pretend-storage*)) (symbol-value sym)))
+    (let ((inner (funcall app env)))
+      (setf (gethash :output (car *pretend-storage*)) inner)
+      inner)))
 
 (defun last-input (&optional (index 0))
   (gethash :input (elt *pretend-storage* index)))
@@ -51,10 +66,12 @@
            (lack.builder:builder
             ,@(subseq middles-and-app insert)))
      (lack.builder:builder
-      ,@(cat
+      ,@(concatenate 'list
          (subseq middles-and-app 0 insert)
-         `((make-instance 'clack-pretend ,@(when watch-symbols
-                                  (list :watch-symbols watch-symbols))))
+         `((lambda (app)
+             (pretend-component app ',watch-symbols)))
+         ;`((make-instance 'clack-pretend ,@(when watch-symbols
+         ;                        (list :watch-symbols watch-symbols))))
          (subseq middles-and-app insert)))))
 
 (defun run-pretend (&optional (index 0))
